6545559eefad716dd1eb0bc06d946cd002a72f2a
Sunnah <suh3@hi.is>
2015-11-13 21:21:04 +0000

    get birt sidurnar i minar sidur

--- routes/find.js
+++ routes/find.js
@@ -2,10 +2,21 @@
 
 var express = require('express');
 var router = express.Router();
+var sites = require('../lib/sites');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('find', { title: 'Express' });
+  sites.gef('suh3', function (err, entryList) {
+  	console.log(entryList);
+    res.render('find', {
+      entries: entryList
+    });
+  });
 });
+
+router.post('/', function(req, res) {
+	console.log(req + ' gunnsteinn segir hæ');
+});
+
 
 module.exports = router;
