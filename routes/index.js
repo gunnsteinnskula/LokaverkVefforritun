3b64f835bdb09f33139b72799707cea6d1b6a420
Gunnsteinn Aron Skúlason <gunnsteinnskula@gmail.com>
2015-11-17 17:07:26 +0000

    ok

--- routes/index.js
+++ routes/index.js
@@ -10,12 +10,18 @@ router.get('/logout', logoutHandler);
 
 
 function loggedInOrNot(req, res, next) {
-  if (req.session.user) {
-  	var user=req.session.user;
-    res.render('index', user );
-  } else {
-    res.render('index', {title: 'Express'});
-  }
+	if (req.session.user) {
+		var user=req.session.user;
+		users.listFriends(user.username, false, function(err, results){
+			res.render('index', {
+				user:user,
+				requests:results
+			});
+		});
+	}
+	else {
+		res.render('index', {title: 'Express'});
+	}
 }
 
 function loginHandler(req, res, next) {
