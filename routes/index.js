2342ac08eb55266d0aa763323d58585ce3523ec0
Sunnah <suh3@hi.is>
2015-11-17 17:04:09 +0000

    byrtir vinabeidnirnar

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@
 
 var express = require('express');
 var router = express.Router();
-var user = require('../lib/users');
+var users=require('../lib/users');
 /* GET home page. */
 router.get('/', loggedInOrNot);
 
@@ -10,8 +10,14 @@ router.get('/', loggedInOrNot);
 function loggedInOrNot(req, res, next) {
   if (req.session.user) {
   	var user=req.session.user;
-    res.render('index', user );
-  } else {
+  	users.listFriends(user.username, false, function(err, results){
+  		res.render('index', {
+  			user:user,
+  			requests:results
+  		});
+  	});
+  }
+  else {
     res.render('index', {title: 'Express'});
   }
 }
