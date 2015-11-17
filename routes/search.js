69376c8ddea48dec3eaa043152a477a7e433bf5b
Sunnah <suh3@hi.is>
2015-11-17 16:12:28 +0000

    haegt ad adda vinum og ekki haegt ad adda sama vini oftar en 1x

--- routes/search.js
+++ routes/search.js
@@ -2,11 +2,26 @@
 
 var express = require('express');
 var router = express.Router();
-var friends = require('../lib/friends');
+var friends = require('../lib/users');
 
 router.get('/', function(req, res) {
 	res.render('search', { title:'Express'});
 });
- 
+
+router.post('/', function(req, res) {
+	friends.addFriend(req.session.user.username, req.body.friendName , function (err, status) {
+    if (err) {
+      console.error(err);
+    }
+
+    var success = true;
+
+    if (err || !status) {
+      success = false;
+    }
+
+    return res.render('search', { title: 'Create user', post: true, success: success })
+  });
+});
 
 module.exports = router;
